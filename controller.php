2a9e6820d4179b3503a3b6d1b15cf67590b10855
suuuuu017 <suuuuu0177@outlook.com>
2023-11-01 17:51:34 -0400

    delete all empty lines cauase i have ocd

--- controller.php
+++ controller.php
@@ -290,83 +290,55 @@ class controller {
 
 
     public function validateRegistry() {
-
-
         $m1 = "";
-
         if ($_POST['email'] != "") {
-
             if ($this->is_uva_email($_POST['email']) !== 1) {
                 $m1 .= "The email is not a valid uva email, make sure the domain is @virginia.edu <br>";
-
             }
-
         }
-
         else {
             $m1 .= "No email address was provided <br>";
         }
-
-
-
         if ($_POST['username'] == "") {
-
             $m1 .= "No username was provided <br>";
         }
-
         if ($_POST['password1'] != "" && $_POST['password2'] != "") {
-
             if ($_POST['password1'] !== $_POST['password2']) {
                 $m1 .= "Your passwords didn't match<br>";
             }
-
             else {
-
                 if (!$this->verifyPassword($_POST['password1'])) {
                     $m1 .= "Your password didn't contain at Minimum eight characters, at least one letter, one number and one special character <br>";
                 }
             }
         }
-
         else {
             $m1 .= "You didn't enter your password at twice<br>";
         }
-
         if ( $m1 === "") {
              // SAVE ALL OF THE CONTENT
              $this->showMainPage();
         }
-
         else {
-            
             $message = "<div class=\"alert alert-danger\" role=\"alert\">
             $m1 </div>";
-
             $this->register($message);
-
         }
     }
 
 
     function is_uva_email($email) {
-
         $email_regex =  $email_regex = '/^[A-Za-z0-9]+@virginia.edu$/';
-        
         return preg_match($email_regex, $email);
-    
     }
     
     
     function verifyPassword($password1) {
-    
         // Minimum eight characters, at least one letter, one number and one special character:
-        
         $password_regex = '/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/';
-    
         if (preg_match($password_regex, $password1) === 1) {
                 return true;
         }
-    
         return false;
     
     }
@@ -377,7 +349,6 @@ class controller {
             $_SESSION['userName'] = $_POST['userName'];
             $_SESSION['description'] = $_POST['description'];
         // }
-
         include ('profile.php');
     }
 
