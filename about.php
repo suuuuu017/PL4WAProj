6b2426991b163dc4e005a3642727dd80a7f977a9
suuuuu017 <suuuuu0177@outlook.com>
2023-11-02 15:44:27 -0400

    add about page and related fucntions back

--- about.php
+++ about.php
@@ -134,7 +134,7 @@
         <div>
             <h1 class="float-md-start mb-0">HoosHiking</h1>
             <nav class="nav nav-masthead justify-content-center float-md-end ">
-                <a type="submit" class="nav-link fw-bold py-1 px-0 " href="">Home</a>
+                <a type="submit" class="nav-link fw-bold py-1 px-0 " href="?command=welcome">Home</a>
                 <a type="submit" class="nav-link fw-bold py-1 px-0" href="">Profile</a>
                 <a type="submit" class="nav-link fw-bold py-1 px-0" href="?command=logout">Logout</a>
                 <!--        <a class="nav-link fw-bold py-1 px-0" href="#">Contact</a>-->
